a8432458838ca9201043b0de5045acd95cda9939
Taehoon <cjsxogns0518@gmail.com>
2025-06-22 01:16:41 +0900

    1

---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/cth")
+    public String cth() {
+        return "cth";
+    }
+}
